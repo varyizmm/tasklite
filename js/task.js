c44ee8a302a7e0c1bcd5b5a0be9015cd9a674034
Варя <you@example.com>
2026-04-08 18:57:04 +0300

    style(tabs): оформление вкладок и их состояний

---
+++ js/task.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 5;
+let sum = a + b;
+console.log(sum);
+let difference = a - b;
+console.log(difference);
+let name = `Varvara`;
+let lastname = `Anosova`;
+let fullname = name + ` ` + lastname
+console. log(fullname)
